7064006714a1135e4ab483197d93a092577a96fd
Hagen DeRouen <hagend87@gmail.com>
2021-05-21 13:16:23 -0500

    Update get and post workout routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,7 @@ const { Workout, Exercise } = require('../../models');
 
 router.get('/workouts', async (req, res) => {
     try {
-        const workouts = await Workout.find();
+        const workouts = await Workout.find().sort('day');
         console.log(workouts);
         res.json(workouts);
     } catch (error) {
@@ -15,7 +15,7 @@ router.get('/workouts', async (req, res) => {
 router.put('/workouts/:id', async (req, res) => {
     try {
         const updatedWorkout = await Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } });
-        res.status.json(updatedWorkout);
+        res.json(updatedWorkout);
     } catch (error) {
         console.log(error);
         res.status(500).json(error);
@@ -24,8 +24,23 @@ router.put('/workouts/:id', async (req, res) => {
 
 router.post('/workouts', async (req, res) => {
     try {
-        const newWorkout = await Workout.create(req.body);
-        res.json(newWorkout);
+        const newWorkout = new Workout({
+            ...req.body,
+            day: new Date().setDate(new Date().getDate()-10)
+        });
+        const savedWorkout = await newWorkout.save();
+        res.json(savedWorkout);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+router.get('/workouts/range', async (req, res) => {
+    try {
+        const workouts = await Workout.find();
+        console.log(workouts);
+        res.json(workouts);
     } catch (error) {
         console.log(error);
         res.status(500).json(error);
